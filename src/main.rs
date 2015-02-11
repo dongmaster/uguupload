2e638f10d1df0a0841796264b00ad14a6729a63b
Dongmaster <dongmaster@users.noreply.github.com>
2015-02-11 15:56:08 +0100

    Reversed flag usage. Use -f if you want to specify filenames for files. No -f flag means that you just have to type in files, not filenames.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ fn main() {
     let shitty_args = env::args();
     let mut args = Vec::new();
     let mut counter = 0;
+    // I use this to get the file. Check line ~65.
+    // If this is 0, then the filename will be the name of the file
+    let mut file_index = 0;
+    let mut start_index = 1;
+    let mut if_index = 0;
     
     // I want to be able to index arguments so I push them to a vector.
     for x in shitty_args {
@@ -31,35 +36,53 @@ If you want to know how to use this program, use the --help flag (./uguupload --
     if args[1] == "--help" {
         println!("Uguupload - A program for uploading files to http://uguu.se
 
-Usage: ./uguupload [FILENAME] [FILE]
+Usage: ./uguupload [options] [FILE]
 
 [FILENAME] is the name you want for the file.
 [FILE] is the path to the file you want to upload.
 
 Uploading several files at once is possible. See Example 2 below.
 
-Please note that when supplying a filename for your file, filetypes can matter.
-So if you upload foo.txt with the name foo, your browser will most likely prompt the person visiting the file with a download dialog window.
+OPTIONS
+    -f, --filenames
+        Allows you to specify filenames for files.
+        See Example 3.
 
-Example:
-./uguupload foo foo.txt
-This will return http://a.uguu.se/1234567890_[FILENAME]
+EXAMPLES
+    Example 1:
+        ./uguupload foo.txt
+        This will return http://a.uguu.se/sdfyse_[FILE]
 
-Example 2:
-./uguupload foo foo.txt bar bar.txt foobar foobar.txt");
+    Example 2:
+        ./uguupload foo.txt bar.txt foobar.txt
+        This will return three (3) files
+
+    Example 3:
+        ./uguupload -f foo.txt foo.txt bar.txt foo.txt foobar.txt foo.txt
+        
+        The first argument, foo.txt is the filename and the second argument, foo.txt, is the file and so on.
+        bar.txt is the filename and foo.txt is the file. Same for foobar.txt and foo.txt
+        
+        Syntax in other words:
+        ./uguupload -f [FILENAME] [FILE]");
 
         return;
+    } else if args[1] == "-f" || args[1] == "--filenames" {
+        // Getting this to work can most likely be done in a better way.
+        file_index = 1;
+        start_index = 2;
+        if_index = 1;
     } else if args_len == 2 {
         panic!("\n\nYou fucked up! Not enough arguments were supplied.
 If you want to know how to use this program, use the --help flag (./uguupload --help)\n\n");
     }
     
-    for x in range(1, args_len - 1) {
+    for x in range(start_index, args_len) {
         // Checking stuff so we don't go out of the index.
-        if x + 1 + counter < args_len {
+        if x + if_index + counter < args_len {
             let filename = format!("name={}", args[x + counter]);
-            let file = format!("file=@{}", args[x + 1 + counter]);
-        
+            let file = format!("file=@{}", args[x + file_index + counter]);
+            
             let mut curl = old_io::Command::new("curl");
                 curl.args(&["-F", filename.as_slice()]);
                 curl.args(&["-F", file.as_slice()]);
@@ -68,9 +91,13 @@ If you want to know how to use this program, use the --help flag (./uguupload --
             match curl.output() {
                 Ok(r)   => {
                     if String::from_utf8_lossy(r.output.as_slice()) == "" {
-                        println!("Upload failed.");
+                        if file_index == 1 {
+                            println!("Upload failed! {}:{} (filename:file) didn't upload successfully.", args[x + counter], args[x + file_index + counter]);
+                        } else {
+                            println!("Upload failed! {} didn't upload successfully.", args[x + file_index + counter]);
+                        }
                     } else {
-                        println!("Looks like your shitty file was uploaded. Here's the link to your file: \n{}", String::from_utf8_lossy(r.output.as_slice()));
+                        println!("Looks like your shitty file was uploaded. Here's the link to your file: {}", String::from_utf8_lossy(r.output.as_slice()));
                     }
                 },
                 Err(e)  => panic!("failed to upload the file: {}", e),
@@ -78,6 +105,8 @@ If you want to know how to use this program, use the --help flag (./uguupload --
         
         }
         
-        counter += 1;
+        if file_index == 1 {
+            counter += 1;
+        }
     }
 }
