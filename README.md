1778c91ad3ea134686ec502c29421fac25cc3991
dongmaster <dongmaster@users.noreply.github.com>
2015-05-06 09:13:46 +0200

    Rewrote code, added and changed flags. New flag: -d. Removed flag: -R (it is now -dr). It is now compilable on the latest nightly.

--- README.md
+++ README.md
@@ -14,26 +14,34 @@ uguu~
 (http://i.imgur.com/LRhtsze.jpg)
 
 ## Usage
-(This help message may or may not be up-to-date.)
 ```./uguupload --help```
 
 ```
-Usage: ./uguupload [options] [FILE]
+Usage: ./uguupload [option] [FILE]
 
 [FILE] is the path to the file you want to upload.
 
 OPTIONS
-    -f, --filenames
-        Allows you to specify filenames for files.
+    -h, --help
+        Prints out this help message.
+
+    -f, --filename
+        Allows you to specify custom filenames for files.
         See Example 2.
     
     -r, --random
-        Gives you random filenames.
+        Gives files random filenames.
         See Example 3.
         
-    -R, --recursive
-        Uploads everything in a directory.
+    -d, --dir
+        Uploads everything in the specified directory.
+        This is not recursive.
         See Example 4.
+        
+    -dr, --dir-rec
+        Uploads everything in the specified directory.
+        This is recursive. Files in sub-directories will be uploaded.
+        See Example 4. This works exactly like -d.
 
 EXAMPLES
     Example 1:
@@ -44,7 +52,7 @@ EXAMPLES
         ./uguupload foo.txt bar.txt foobar.txt
 
     Example 2:
-        ./uguupload -f [FILENAME] [FILE]
+        ./uguupload -f [FILE] [FILENAME]
         ./uguupload -f foo.txt foo.txt bar.txt foo.txt foobar.txt foo.txt
         
         The first argument, foo.txt is the filename and the second argument, foo.txt, is the file and so on.
@@ -57,7 +65,7 @@ EXAMPLES
         This will return http://a.uguu.se/asdfghjkl.ext where .ext is the file extension/type
         
     Example 4:
-        ./uguupload -R [DIRECTORY]
+        ./uguupload -d [DIRECTORY]
         This will upload everything in the specified directory. You can add several directories
 ```
 
@@ -70,7 +78,7 @@ Your package manager (99% sure) has curl. I recommend installing it that way :>.
 You need to have [Rust](http://rust-lang.org) installed to compile this. I assume you have cargo installed as well (If you don't, you should just be able to do ```rustc main.rs``` and it should compile).
 
 ```
-git clone https://github.com/dongmaster/uguupload && cd uguupload && cargo build
+git clone https://github.com/dongmaster/uguupload && cd uguupload && cargo build --release
 ```
 
 ## Supported platforms
--- src/main.rs
+++ src/main.rs
@@ -5,58 +5,196 @@
  *  or http://opensource.org/licenses/BSD-2-Clause
  * */
 
-#![feature(env)]
-#![feature(core)]
-#![feature(old_io)]
-#![feature(old_path)]
+#![feature(fs_walk)]
+#![feature(path_ext)]
 
 use std::env;
-use std::old_io;
-use std::old_io::fs;
-use std::old_io::fs::PathExtensions;
+use std::process;
+use std::fs;
+use std::path::Path;
+use std::fs::PathExt;
+
+const URL : &'static str = "http://uguu.se/api.php?d=upload";
 
 fn main() {
-    let upload_url = "http://uguu.se/api.php?d=upload";
-    let shitty_args = env::args();
-    let mut args = Vec::new();
-    let counter: usize = 0;
+    let args: Vec<_> = env::args().collect();
     
-    // I want to be able to index arguments so I push them to a vector.
-    for x in shitty_args {
-        /*
-        match x.into_string() {
-            Ok(r)   => args.push(r),
-            Err(e)  => panic!("Something went wrong. It has something to do with command-line arguments. Error: {:?}", e),
+    handle_arguments(args);
+}
+
+fn handle_arguments(args: Vec<String>) {
+    // Handles command-line arguments
+
+    let arg = args[1].clone();
+
+    match arg.as_ref() {
+        "-h" | "--help"     => help(),
+        "-f" | "--filename" => upload_f(args),
+        "-r" | "--random"   => upload_r(args),
+        "-d" | "--dir"      => upload_d(args),
+        "-dr" | "--dir-rec" => upload_dr(args),
+        _                   => upload_n(args),
+    }
+}
+
+fn upload(f: &String, file: String, filename: String, random: bool) {
+    // Handles the uploading of files.
+    let mut curl = process::Command::new("curl");
+            curl.args(&["-F", filename.as_ref()]);
+            curl.args(&["-F", file.as_ref()]);
+            if random == true {
+                curl.args(&["-F", "randomname=true"]);
+            }
+            curl.arg(URL);
+            
+    match curl.output() {
+        Ok(r)   => {
+            if String::from_utf8_lossy(r.stdout.as_ref()) == "" {
+                println!("Failed to upload: {}", f);
+            } else {
+                println!("{}\n{}", f, String::from_utf8_lossy(r.stdout.as_ref()));
+            }
+        },
+        Err(e)  => panic!("Failed to upload: {}", e),
+    }
+}
+
+fn upload_n(args: Vec<String>) {
+    // NORMAL
+    // Handles what files should be uploaded along with what filename the file should have.
+    let files = &args[1..args.len()];
+    
+    for f in files {
+        let filename = format!("name={}", f);
+        let file = format!("file=@{}", f);
+        
+        upload(f, file, filename, false);
+    }
+}
+
+fn upload_f(args: Vec<String>) {
+    // FILENAME
+    // Handles what files should be uploaded along with that filename the file should have.
+    // -f was used here.
+    let files = &args[2..args.len()];
+    let mut counter = 0;
+    
+    for x in 0..files.len() {
+        if x + 1 + counter < files.len() { 
+            let filename = format!("name={}", files[x + 1 + counter]);
+            let file = format!("file=@{}", files[x + counter]);
+            
+            upload(&files[x + counter], file, filename, false);
         }
-        * */
-        args.push(x);
+        
+        counter += 1;
     }
+}
+
+fn upload_r(args: Vec<String>) {
+    // RANDOM
+    // Handles what files should be uploaded. Filenames are random.
+    // -r was used here
+    let files = &args[2..args.len()];
     
-    // Efficiency?
-    let args_len: usize = args.len();
-    
-    if args_len < 2 {
-        panic!("\n\nYou fucked up. Not enough arguments were supplied.
-If you want to know how to use this program, use the --help flag (./uguupload --help)\n\n");
+    for f in files {
+        let filename = format!("name={}", f);
+        let file = format!("file=@{}", f);
+        
+        upload(f, file, filename, true);
     }
+}
+
+fn upload_d(args: Vec<String>) {
+    // DIRECTORY
+    // Handles what files in the directory should be uploaded.
+    // -d was used here.
     
-    if args[1] == "--help" {
-        println!("Usage: ./uguupload [options] [FILE]
+    let directories = &args[2..args.len()];
+    
+    for d in directories {
+        let path = Path::new(d);
+        let mut fls: Vec<_> = vec!();
+        
+        if path.is_dir() == true {
+            match fs::read_dir(&path) {
+                Err(why) => println!("! {:?}", why.kind()),
+                Ok(paths) => for path in paths {
+                    fls.push(path.unwrap().path());
+                },
+            }
+            
+            for x in fls {
+                let filename_processed = x.as_path().file_name().unwrap().to_str().unwrap().to_string();
+                let file_processed = x.as_path().to_str().unwrap().to_string();
+                
+                let filename = format!("name={}", filename_processed);
+                let file = format!("file=@{}", file_processed);
+                
+                upload(&file_processed, file, filename, false);
+            }
+        }
+    }
+}
+
+fn upload_dr(args: Vec<String>) {
+    // DIRECTORY
+    // Handles what files in the directory should be uploaded.
+    // -d was used here.
+    
+    let directories = &args[2..args.len()];
+    
+    for d in directories {
+        let path = Path::new(d);
+        let mut fls: Vec<_> = vec!();
+        
+        if path.is_dir() == true {
+            match fs::walk_dir(&path) {
+                Err(why) => println!("! {:?}", why.kind()),
+                Ok(paths) => for path in paths {
+                    fls.push(path.unwrap().path());
+                },
+            }
+            
+            for x in fls {
+                let filename_processed = x.as_path().file_name().unwrap().to_str().unwrap().to_string();
+                let file_processed = x.as_path().to_str().unwrap().to_string();
+                
+                let filename = format!("name={}", filename_processed);
+                let file = format!("file=@{}", file_processed);
+                
+                upload(&file_processed, file, filename, false);
+            }
+        }
+    }
+}
+
+fn help() {
+    println!("Usage: ./uguupload [option] [FILE]
 
 [FILE] is the path to the file you want to upload.
 
 OPTIONS
-    -f, --filenames
-        Allows you to specify filenames for files.
+    -h, --help
+        Prints out this help message.
+
+    -f, --filename
+        Allows you to specify custom filenames for files.
         See Example 2.
     
     -r, --random
-        Gives you random filenames.
+        Gives files random filenames.
         See Example 3.
         
-    -R, --recursive
-        Uploads everything in a directory.
+    -d, --dir
+        Uploads everything in the specified directory.
+        This is not recursive.
         See Example 4.
+        
+    -dr, --dir-rec
+        Uploads everything in the specified directory.
+        This is recursive. Files in sub-directories will be uploaded.
+        See Example 4. This works exactly like -d.
 
 EXAMPLES
     Example 1:
@@ -67,7 +205,7 @@ EXAMPLES
         ./uguupload foo.txt bar.txt foobar.txt
 
     Example 2:
-        ./uguupload -f [FILENAME] [FILE]
+        ./uguupload -f [FILE] [FILENAME]
         ./uguupload -f foo.txt foo.txt bar.txt foo.txt foobar.txt foo.txt
         
         The first argument, foo.txt is the filename and the second argument, foo.txt, is the file and so on.
@@ -80,170 +218,8 @@ EXAMPLES
         This will return http://a.uguu.se/asdfghjkl.ext where .ext is the file extension/type
         
     Example 4:
-        ./uguupload -R [DIRECTORY]
+        ./uguupload -d [DIRECTORY]
         This will upload everything in the specified directory. You can add several directories");
-
-        return;
-    } else if args[1] == "-f" || args[1] == "--filename" {
-        upload_files_filenames(upload_url, args, args_len, counter);
-    } else if args[1] == "-r" || args[1] == "--random" {
-        upload_files_random_filenames(upload_url, args, args_len, counter);
-    } else if args[1] == "-R" || args[1] == "--recursive" {
-        for d in range(2, args_len) {
-            let ref dir = args[d];
-            
-            let path = Path::new(dir);
-            
-            if path.is_dir() == true {
-                let contents = match fs::walk_dir(&path) {
-                    Ok(t)   => t,
-                    Err(e)  => panic!("\n\nFailed to crawl the specified directory, error: {}\n\n", e),
-                };
-                
-                let mut files = Vec::new();
-                let mut files_str = Vec::new();
-                
-                for x in contents {
-                    if x.is_file() == true {
-                        files.push(x);
-                    }
-                }
-                
-                for y in files.iter() {
-                    match y.as_str() {
-                        Some(t) => { 
-                            match y.filename_str() {
-                                Some(n) => files_str.push(n),
-                                None    => {},
-                            }
-                            
-                            files_str.push(t)
-                        },
-                        None    => {},
-                    };
-                }
-                
-                let files_len: usize = files_str.len();
-                
-                upload_files_recursive(upload_url, files_str, files_len, counter);
-            }
-        }
         
-    } else {
-        upload_files(upload_url, args, args_len, counter);
-    }
+    return;
 }
-
-// Upload files where the name of the file is the filename.
-fn upload_files(upload_url: &str, args: Vec<String>, args_len: usize, counter: usize) {
-    for x in range(1, args_len) {
-        // Checking stuff so we don't go out of the index.
-        if x + counter < args_len {
-            let filename = format!("name={}", args[x + counter]);
-            let file = format!("file=@{}", args[x + counter]);
-            
-            let mut curl = old_io::Command::new("curl");
-                curl.args(&["-F", filename.as_slice()]);
-                curl.args(&["-F", file.as_slice()]);
-                curl.arg(upload_url);
-            
-            match curl.output() {
-                Ok(r)   => {
-                    if String::from_utf8_lossy(r.output.as_slice()) == "" {
-                        println!("Failed to upload: {}", args[x + counter]);
-                    } else {
-                        println!("\nSuccessfully uploaded {}\n{}", args[x + counter], String::from_utf8_lossy(r.output.as_slice()));
-                    }
-                },
-                Err(e)  => panic!("Failed to upload: {}", e),
-            }
-        }
-    }
-}
-
-//Upload files with user-specified filenames
-fn upload_files_filenames(upload_url: &str, args: Vec<String>, args_len: usize, mut counter: usize) {
-    // The index starts at two (2) because of the flag
-    for x in range(2, args_len) {
-        // Checking stuff so we don't go out of the index.
-        if x + 1 + counter < args_len {
-            let filename = format!("name={}", args[x + counter]);
-            let file = format!("file=@{}", args[x + 1 + counter]);
-            
-            let mut curl = old_io::Command::new("curl");
-                curl.args(&["-F", filename.as_slice()]);
-                curl.args(&["-F", file.as_slice()]);
-                curl.arg(upload_url);
-            
-            match curl.output() {
-                Ok(r)   => {
-                    if String::from_utf8_lossy(r.output.as_slice()) == "" {
-                        println!("Failed to upload {} - {} (filename:file)", args[x + counter], args[x + 1 + counter]);
-                    } else {
-                        println!("\nSuccessfully uploaded {} - {} (filename:file)\n{}", args[x + counter], args[x + 1 + counter], String::from_utf8_lossy(r.output.as_slice()));
-                    }
-                },
-                Err(e)  => panic!("Failed to upload: {}", e),
-            }
-        }
-        
-        counter += 1;
-    }
-}
-
-fn upload_files_random_filenames(upload_url: &str, args: Vec<String>, args_len: usize, counter: usize) {    
-    for x in range(2, args_len) {
-        // Checking stuff so we don't go out of the index.
-        if x + counter < args_len {
-            let filename = format!("name={}", args[x + counter]);
-            let file = format!("file=@{}", args[x + counter]);
-            
-            let mut curl = old_io::Command::new("curl");
-                curl.args(&["-F", filename.as_slice()]);
-                curl.args(&["-F", file.as_slice()]);
-                curl.args(&["-F", "randomname=true"]);
-                curl.arg(upload_url);
-            
-            match curl.output() {
-                Ok(r)   => {
-                    if String::from_utf8_lossy(r.output.as_slice()) == "" {
-                        println!("Failed to upload: {}", args[x + counter]);
-                    } else {
-                        println!("\nSuccessfully uploaded {}\n{}", args[x + counter], String::from_utf8_lossy(r.output.as_slice()));
-                    }
-                },
-                Err(e)  => panic!("Failed to upload: {}", e),
-            }
-        }
-    }
-}
-
-fn upload_files_recursive(upload_url: &str, args: Vec<&str>, files_len: usize, mut counter: usize) {
-    // The index starts at two (2) because of the flag
-    for x in range(0, files_len) {
-        // Checking stuff so we don't go out of the index.
-        if x + 1 + counter < files_len {
-            let filename = format!("name={}", args[x + counter]);
-            let file = format!("file=@{}", args[x + 1 + counter]);
-            
-            let mut curl = old_io::Command::new("curl");
-                curl.args(&["-F", filename.as_slice()]);
-                curl.args(&["-F", file.as_slice()]);
-                curl.arg(upload_url);
-            
-            match curl.output() {
-                Ok(r)   => {
-                    if String::from_utf8_lossy(r.output.as_slice()) == "" {
-                        println!("Failed to upload {} - {} (filename:file)", args[x + counter], args[x + 1 + counter]);
-                    } else {
-                        println!("\nSuccessfully uploaded {} - {} (filename:file)\n{}", args[x + counter], args[x + 1 + counter], String::from_utf8_lossy(r.output.as_slice()));
-                    }
-                },
-                Err(e)  => panic!("Failed to upload: {}", e),
-            }
-        }
-        
-        counter += 1;
-    }
-}
-
